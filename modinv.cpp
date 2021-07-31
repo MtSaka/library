bf820d7fff64184750577e3ca5200c683157e04b
MtSaka <76619232+MtSaka@users.noreply.github.com>
2021-07-31 22:22:48 +0100

    Update modinv.cpp

--- modinv.cpp
+++ modinv.cpp
@@ -1,18 +1,4 @@
-#include<bits/stdc++.h>
-#define ll long long
 ll modinv(ll a,ll m){
-  ll b=m,x=1,y=0;
-  while(b>0){
-    x-=y*(a/b);
-    swap(x,y);
-    a=a%b;
-    swap(a,b);
-  }
-  x=x%m; 
-  if(x>=0){
-    return x;
-  }
-  else{
-    return x+m;
-  }
+  if(a==1){return 1;}
+  return m-modinv(m%a,m)*(m/a)%m;
 }
