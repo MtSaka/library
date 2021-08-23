f71eb51627f9ed99c6d42ca67efc779fa891b49d
MtSaka <76619232+MtSaka@users.noreply.github.com>
2021-08-23 11:33:55 +0100

    Add files via upload

---
+++ Data_Structure/inversion.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include"../../library/Data_Structure/BIT.cpp"
+using namespace std;
+template<typename T>
+long long inversion(vector<T>&a){
+  int n=a.size();
+  vector<T>b=a;
+  sort(b.begin(),b.end());
+  map<long long,long long>mp;
+  for(int i=0;i<n;i++)mp[b[i]]=i+1;
+  for(int i=0;i<n;i++)a[i]=mp[a[i]];
+  long long ans=0;
+  BIT<long long>c(n);
+  for(int i=0;i<n;i++){
+    ans+=i-c.sum(a[i]);
+    c.add(a[i],1);
+  }
+  return ans;
+}
