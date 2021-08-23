c387d7211ecf8027a3dd3a84816399b09d8e12a0
MtSaka <76619232+MtSaka@users.noreply.github.com>
2021-08-23 21:16:28 +0100

    Add files via upload

---
+++ Dynamic_programming/count-subsequences.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long subcount(string s){
+  int n=s.size();
+  vector<vector<int>>next(n+1,vector<int>(26,0));
+  for(int i=n-1;i>=0;i--){
+    for(int j=0;j<26;j++)next[i][j]=next[i+1][j];
+    next[i][s[i]-'a']=i;
+  }
+  vector<long long>dp(n+1,0);
+  dp[0]=1;
+  for(int i=0;i<n;i++)for(int j=0;j<26;j++)if(next[i][j]<n)dp[next[i][j]+1]=dp[next[i][j]+1]+dp[i];
+  long long ans=0;
+  for(int i=0;i<=n;i++)ans+=dp[i];
+  return ans;
+}
