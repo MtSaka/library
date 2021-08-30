40a37e42b9399d72467a080f08b0decbb1728d2d
MtSaka <76619232+MtSaka@users.noreply.github.com>
2021-08-30 17:35:04 +0100

    Update modint.cpp

--- Math/modint.cpp
+++ Math/modint.cpp
@@ -4,42 +4,39 @@ template<long long m>
 struct modint{
   long long x;
   modint():x(0){}
-  modint(long long y):x(y>=0?y%m:(m+y%m)%m){}
-  modint inv()const{
-    long long a=x,b=m,u=1,v=0,t;
-    while(b>0){
-      t-=a/b;
+  modint(long long y):x(y>=0?y%m:(m-(-y)%m)%m){}
+  modint inv()const{long long a=x,b=m,u=1,v=0,t;
+    while(b){
+      t=a/b;
       swap(a-=t*b,b);
       swap(u-=t*v,v);
     }
     return modint(u);
   }
-  modint &operator+=(const modint&p){if((x+=p.x)>=m)x-=m;return *this;}
+  modint &operator+=(const modint&p) {if((x+=p.x)>=m)x-=m;return *this;}
   modint &operator-=(const modint&p){if((x+=m-p.x)>=m)x-=m;return *this;}
-  modint &operator*=(const modint&p){x=(x*p.x)%m;return *this;}
+  modint &operator*=(const modint&p) {x=x*p.x%m;return *this;}
   modint &operator/=(const modint&p){*this*=p.inv();return *this;}
-  modint operator-() const{return modint(-x);}
-  modint operator+(const modint&p)const{return modint(*this)+=p;}
-  modint operator-(const modint&p)const{return modint(*this)-=p;}
-  modint operator*(const modint&p)const{return modint(*this)*=p;}
-  modint operator/(const modint&p)const{return modint(*this)/=p;}
-  modint &operator++(int){if(x++==m)x=0;return *this;}
-  modint &operator--(int){if(x--<0)x+=m;return *this;}
+  modint operator-()const{return modint(-x); }
+  modint operator+(const modint&p)const{return modint(*this)+=p; }
+  modint operator-(const modint&p)const{return modint(*this)-=p; }
+  modint operator*(const modint&p)const{return modint(*this)*=p; }
+  modint operator/(const modint&p)const{return modint(*this)/=p; }
   bool operator==(const modint&p)const{return x==p.x;}
   bool operator!=(const modint&p)const{return x!=p.x;}
-  modint pow(long long n)const{
+  modint pow(long long n) const {
     modint ret(1),mul(x);
-    while(n>0){
+    while(n){
       if(n&1)ret*=mul;
       mul*=mul;
       n>>=1;
     }
     return ret;
   }
-  friend ostream&operator<<(ostream&os,const modint&p){
+  friend ostream &operator<<(ostream &os,const modint&p) {
     return os<<p.x;
   }
-  friend istream&operator>>(istream&is,modint&a){
+  friend istream &operator>>(istream &is, modint &a) {
     long long t;
     is>>t;
     a=modint<m>(t);
