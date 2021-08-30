edc7aa8890c7ed3c1efae1b14eaabe82935a5583
MtSaka <76619232+MtSaka@users.noreply.github.com>
2021-08-30 18:03:54 +0100

    Add files via upload

---
+++ Math/lagrange.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<../../code/library/Math/com.cpp>
+using namespace std;
+template<long long m>
+modint<m> lagrange_polynominal(vector<modint<m>>&y,long long t){
+  using mint=modint<m>;
+  long long n=y.size()-1;
+  combination<m>c(n);
+  if(t<=n)return y[t];
+  mint ret;
+  vector<mint>dp(n+1,1),pd(n+1,1);
+  for(int i=0;i<n;i++)dp[i+1]=dp[i]*(t-i);
+  for(int i=n;i>0;i--)pd[i-1]=pd[i]*(t-i);
+  for(int i=0;i<=n;i++){
+    mint tmp=y[i]*dp[i]*pd[i]*c.idat[i]*c.idat[n-i];
+    if((n-i)&1)ret-=tmp;
+    else ret+=tmp;
+  }
+  return ret;
+}
