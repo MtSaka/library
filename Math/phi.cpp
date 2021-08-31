bd72cc80da16a8ca76ff8064c645e46a3b3e2a69
MtSaka <76619232+MtSaka@users.noreply.github.com>
2021-08-31 19:09:01 +0100

    Add files via upload

---
+++ Math/phi.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+long long phi(long long n){
+  long long ans=n;
+  for(long long i=2;i*i<=n;i++){
+    if(n%i==0){
+      ans/=i;
+      ans*=i-1;
+      while(n%i==0)n/=i;
+    }
+  }
+  if(n>1){
+    ans/=n;
+    ans*=(n-1);
+  }
+  return ans;
+}
