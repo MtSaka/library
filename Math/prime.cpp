26b29032c6494a30e9ae342126354b30743f3be3
MtSaka <mtsakachin@gmail.com>
2021-11-28 11:28:30 +0000

    added prime_sieve and range_prime_sieve

--- Math/prime.cpp
+++ Math/prime.cpp
@@ -49,3 +49,60 @@ long long factor_cnt(long long N){
   if(N!=1)ans*=2;
   return ans;
 }
+template<typename T>
+struct prime_sieve{
+  vector<T>mpf;
+  vector<T>primes;
+  prime_sieve(T n=1e5){
+    mpf.resize(n+1,0);
+    iota(mpf.begin(),mpf.end(),0);
+    mpf[0]=mpf[1]=-1;
+    for(T i=2;i*i<=n;i++){
+      if(mpf[i]==i){
+        for(T j=i*i;j<=n;j+=i){
+          if(mpf[j]==j)mpf[j]=i;
+        }
+      }
+    }
+    for(T i=2;i<=n;i++)if(mpf[i]==i)primes.push_back(i);
+  }
+  vector<T>factorize(T n){
+    vector<T>res;
+    T now=n;
+    while(now!=1){
+      res.push_back(mpf[now]);
+      now/=mpf[now];
+    }
+    return res;
+  }
+};
+template<typename T>
+struct range_prime_sieve{
+  T l,r,m;
+  vector<T>small;
+  vector<vector<T>>large;
+  vector<T>mul;
+  range_prime_sieve(T l,T r):l(l),r(r),m(sqrt(r)+1){
+    large.resize(r-l);
+    small.resize(m);
+    mul.resize(r-l);
+    iota(mul.begin(),mul.end(),l);
+    iota(small.begin(),small.end(),0);
+    for(T i=2;i*i<r;i++){
+      if(small[i]==i){
+        for(T j=i*i;j<m;j+=i)if(small[j]==j)small[j]=i;
+        for(T j=(l+i-1)/i*i;j<r;j+=i){
+          while(mul[j-l]%i==0){
+            mul[j-l]/=i;
+            large[j-l].push_back(i);
+          }
+        }
+      }
+    }
+  }
+  vector<T>factor(T n){
+    vector<T>res=large[n-l];
+    if(mul[n-l]!=1)res.push_back(mul[n-l]);
+    return res;
+  }
+};
