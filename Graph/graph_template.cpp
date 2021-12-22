9656f3b6b2c3dee07f90c16cdaaf75b38f354dab
MtSaka <mtsakachin@gmail.com>
2021-12-22 23:27:34 +0000

    added graph_template

---
+++ Graph/graph_template.cpp
@@ -0,0 +1,40 @@
+template<typename T=int>
+struct Edge{
+  int from,to;
+  T cost;
+  int idx;
+  Edge(){}
+  Edge(int from,int to,T cost=1,int idx=-1):from(from),to(to),cost(cost),idx(idx){}
+  operator int()const{return to;}
+};
+template<typename T=int>
+struct Graph{
+  vector<vector<Edge>>g;
+  int es;
+  Graph(){}
+  explicit Graph(int n):g(n),es(0){}
+  size_t size()const{return g.size();}
+  size_t edge_size()const{return es;}
+  void add_directed_edge(int from,int to,T cost=1){
+    g[from].emplace_back(from,to,cost,es++);
+  }
+  void add_edge(int from,int to,T cost=1){
+    g[from].emplace_back(from,to,cost,es);
+    g[to].emplace_back(to,from,cost,es++);
+  }
+  inline vector<Edge<T>>&operator[](int idx){return g[idx];}
+  inline const vector<Edge<T>>&operator[](int idx)const{return g[idx];}
+  void read(int m,int padding=-1,bool weighed=false,bool direct=false){
+    int a,b;
+    T c;
+    for(int i=0;i<m;i++){
+      cin>>a>>b;
+      a+=padding;
+      b+=padding;
+      c=1;
+      if(weighed)cin>>c;
+      if(direct)add_directed_edge(a,b,c);
+      else add_edge(a,b,c);
+    }
+  }
+};
