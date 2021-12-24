fa5f5e669848923103e200d2e2d2734a920c721d
MtSaka <mtsakachin@gmail.com>
2021-12-24 23:43:49 +0000

    fixed bug

--- Math/fraction.hpp
+++ Math/fraction.hpp
@@ -6,8 +6,9 @@ struct fraction{
   long long num,den;
   fraction():num(0),den(1){}
   fraction(long long n):num(n),den(1){}
-  fraction(long long n,long long d):num(n),den(d){
-    long long g=gcd(n,d);
+  fraction(long long n,long long d):num(n),den(d){normalize();}
+  void normalize(){
+    long long g=gcd(num,den);
     num/=g;den/=g;
     if(den<0){num=-num;den=-den;}
   }
@@ -18,10 +19,10 @@ struct fraction{
   bool operator==(const fraction &f)const{return num*f.den==den*f.num;}
   bool operator!=(const fraction &f)const{return num*f.den!=den*f.num;}
   fraction operator-()const{return fraction(-num,den);}
-  fraction &operator-=(const fraction &f){num=num*f.den-den*f.num;den*=f.den;return *this;}
-  fraction &operator+=(const fraction &f){num=num*f.den+den*f.num;den*=f.den;return *this;}
-  fraction &operator*=(const fraction &f){num*=f.num;den*=f.den;return *this;}
-  fraction &operator/=(const fraction &f){num*=f.den;den*=f.num;return *this;}
+  fraction &operator-=(const fraction &f){num=num*f.den-den*f.num;den*=f.den;normalize();return *this;}
+  fraction &operator+=(const fraction &f){num=num*f.den+den*f.num;den*=f.den;normalize();return *this;}
+  fraction &operator*=(const fraction &f){num*=f.num;den*=f.den;normalize();return *this;}
+  fraction &operator/=(const fraction &f){num*=f.den;den*=f.num;normalize();return *this;}
   friend fraction operator+(const fraction&lhs,const fraction&rhs){return fraction(lhs)+=rhs;}
   friend fraction operator-(const fraction&lhs,const fraction&rhs){return fraction(lhs)-=rhs;}
   friend fraction operator*(const fraction&lhs,const fraction&rhs){return fraction(lhs)*=rhs;}
