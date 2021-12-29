32700a8bf4dec34a2f041896d232cdb42e54d23c
MtSaka <mtsakachin@gmail.com>
2021-12-29 22:39:40 +0000

    add

---
+++ test/aoj/GRL/GRL_5_C.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_C"
+#include"../../../template/template.hpp"
+#include"../../../Graph/lowest_common_ancestor.hpp"
+int main(){
+  int n;
+  cin>>n;
+  Graph<int>g(n);
+  for(int i=0;i<n;i++){
+    int k;
+    cin>>k;
+    while(k--){
+      int j;
+      cin>>j;
+      g.add_edge(i,j);
+    }
+  }
+  int q;
+  cin>>q;
+  LCA<int>lca(g);
+  while(q--){
+    int a,b;
+    cin>>a>>b;
+    cout<<lca.query(a,b)<<endl;
+  }
+}
