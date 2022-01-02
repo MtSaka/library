74672a41257621771e93d479750bf6a4bc6c9140
MtSaka <mtsakachin@gmail.com>
2022-01-02 21:26:11 +0000

    fix

--- test/yosupo/range_affine_range_sum.test.cpp
+++ test/yosupo/range_affine_range_sum.test.cpp
@@ -3,20 +3,20 @@
 #include"../../Math/modint.hpp"
 #include"../../Data_Structure/lazy_segtree.hpp"
 using mint=modint<mod>;
-using pi=pair< mint, int >;
-using qi=pair< mint, mint >;
-pi op(pi a,pi b){return {a.first+b.first,a.second+b.second};}
-pi mapping(qi a,pi b){return {a.first*b.first+a.second*mint(b.second),b.second};}
+using Pi=pair<mint,int >;
+using qi=pair<mint,mint>;
+Pi op(Pi a,Pi b){return {a.first+b.first,a.second+b.second};}
+Pi mapping(qi a,Pi b){return {a.first*b.first+a.second*mint(b.second),b.second};}
 qi composition(qi b,qi a){return {a.first*b.first,a.second*b.first+b.second};}
-pi e(){return pi(0,0);}
+Pi e(){return Pi(0,0);}
 qi id(){return qi(1,0);}
 int main(){
   INT(n,q);
-  lazy_segtree<pi,op,e,qi,mapping,composition,id>s(n);
+  lazy_segtree<Pi,op,e,qi,mapping,composition,id>s(n);
   rep(i,n){
     mint a;
     scan(a);
-    s.set(i,pi(a,1));
+    s.set(i,Pi(a,1));
   }
   while(q--){
     LL(t);
