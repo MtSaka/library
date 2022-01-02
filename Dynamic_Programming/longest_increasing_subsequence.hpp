ddf787963924b2feca58d0ae132b9d554ebd60ca
MtSaka <mtsakachin@gmail.com>
2022-01-02 21:50:45 +0000

    fi

--- Dynamic_Programming/longest_increasing_subsequence.hpp
+++ Dynamic_Programming/longest_increasing_subsequence.hpp
@@ -6,5 +6,5 @@ int lis(const vector<T>&a){
   int n=a.size();
   vector<long long>dp(n,2e18);
   for(int i=0;i<n;i++)*lower_bound(dp.begin(),dp.end(),a[i])=a[i];
-  return lower_bound(dp.begin(),dp.end(),inf)-dp.begin();
+  return lower_bound(dp.begin(),dp.end(),2e18)-dp.begin();
 }
