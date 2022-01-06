7ed091be1d2f950df1253d4853ae1d7d637b03a4
MtSaka <mtsakachin@gmail.com>
2022-01-06 16:52:40 +0000

    fix test

--- test/yosupo/exp_of_formal_power_series.test.cpp
+++ test/yosupo/exp_of_formal_power_series.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/exp_of_formal_power_series"
 #include"../../template/template.hpp"
-#include"../../math/fps.hpp"
+#include"../../Math/fps.hpp"
 int main(){
   int n;
   cin>>n;
