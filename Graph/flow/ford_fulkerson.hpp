690ac85c26dc0dfd203500cc13b93a6b80a35673
MtSaka <mtsakachin@gmail.com>
2022-01-28 23:42:13 +0000

    add ford fulkerson

---
+++ Graph/flow/ford_fulkerson.hpp
@@ -0,0 +1,43 @@
+#pragma once
+template<typename T>
+struct FordFulkerson{
+  struct edge{
+    int to;
+    T cap;
+    int rev;
+    bool isrev
+    int idx;
+  };
+  const T INF;
+  vector<vector<edge>>g;
+  vector<int>used;
+  int timer;
+  FordFulkerson(int v):INF(numeric_limits<T>::max()/2),g(v),used(v,-1),timer(0){}
+  void add_edge(int from,int to,T cap,int idx=-1){
+    g[from].emplace_back(edge{to,cap,(int)g[to].size(),false,idx});
+    g[to].emplace_back(edge{from,0,(int)g[from].size()-1,true,idx});
+  }
+  T find_path(int idx,const int t,T flow){
+    if(idx==t)return flow;
+    used[idx]=timer;
+    for(auto&e:g[idx]){
+      if(e.cap>0&&used[e.to]!=timer){
+        T d=find_path(e.to,t,min(flow,e.cap));
+        if(d>0){
+          e.cap-=d;
+          g[e.to][e.rev].cap+=d;
+          return d;
+        }
+      }
+    }
+    return 0;
+  }
+  T max_flow(int s,int t){
+    T flow=0;
+    for(T f;(f=find_path(s,t,INF));timer++)flow+=f;
+    return flow;
+  }
+};
+/**
+ * @brief Ford Fulkerson(最大流)
+*/
