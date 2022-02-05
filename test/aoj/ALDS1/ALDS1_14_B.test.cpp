ae9b0da3410f88f7d4022c07d865e8c27c6ab42d
MtSaka <mtsakachin@gmail.com>
2022-02-05 22:41:12 +0000

    change

--- test/aoj/ALDS1/ALDS1_14_B.test.cpp
+++ test/aoj/ALDS1/ALDS1_14_B.test.cpp
@@ -5,10 +5,10 @@ int main(){
   string t,p;
   cin>>t>>p;
   RollingHash rh;
-  auto h=rh.get_hash(t);
-  auto h2=rh.get_hash(p);
+  auto rh1=rh.get_hash(t);
+  auto rh2=rh.get_hash(p);
   for(int i=0;i+p.size()<=t.size();i++){
-    if(h.get_hash(i,i+p.size())==h2.get_all()){
+    if(rh1.get_hash(i,i+p.size())==rh2.get_all()){
       cout<<i<<endl;
     }
   }
