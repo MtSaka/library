0005df9ce9d3dbdf9a0d94201ad7ad6fbdb61543
MtSaka <mtsakachin@gmail.com>
2022-02-05 22:23:51 +0000

    add rolling hash

---
+++ test/aoj/ALDS1/ALDS1_14_B.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_14_B"
+#include"../../../template/template.hpp"
+#include"../../../string/rolling_hash.hpp"
+int main(){
+  string t,p;
+  cin>>t>>p;
+  RollingHash rh;
+  auto h=rh.get_hash(t);
+  auto h2=rh.get_hash(p);
+  for(int i=0;i+p.size()<=t.size();i++){
+    if(h.get_hash(i,i+p.size())==h2.get_all()){
+      cout<<i<<endl;
+    }
+  }
+}
