400323bd8ffcdc3cf4aaed91ad63e02c299ba67f
MtSaka <mtsakachin@gmail.com>
2022-03-14 20:38:41 +0000

    change to long double

--- Geometry/geometry.hpp
+++ Geometry/geometry.hpp
@@ -1,33 +1,33 @@
 #pragma once
 namespace geometry{
-  const double eps=1e-8;
-  const double pi=acos(-1.0);
-  bool equals(double a,double b){return fabs(a-b)<eps;}
-  double torad(double deg){return deg*pi/180.0;}
-  double todeg(double rad){return rad*180.0/pi;}
-  int sign(double a){return (a>eps?1:(a<-eps?-1:0));}
+  const long double eps=1e-9;
+  const long double pi=acos(-1.0);
+  bool equals(long double a,long double b){return fabs(a-b)<eps;}
+  long double torad(long double deg){return deg*pi/180.0;}
+  long double todeg(long double rad){return rad*180.0/pi;}
+  int sign(long double a){return (a>eps?1:(a<-eps?-1:0));}
   struct point{
-    double x,y;
-    point(double x=0.0,double y=0.0):x(x),y(y){}
+    long double x,y;
+    point(long double x=0.0,long double y=0.0):x(x),y(y){}
     point operator-()const{return point(-x,-y);}
     point operator+(const point&p)const{return point(x+p.x,y+p.y);}
     point operator-(const point&p)const{return point(x-p.x,y-p.y);}
-    point operator*(const double&k)const{return point(x*k,y*k);}
-    point operator/(const double&k)const{return point(x/k,y/k);}
+    point operator*(const long double&k)const{return point(x*k,y*k);}
+    point operator/(const long double&k)const{return point(x/k,y/k);}
     point operator*(const point&p)const{return point(x*p.x-y*p.y,x*p.y+y*p.x);}
     point operator/(const point&p)const{return point((x*p.x+y*p.y)/p.norm(),(x*p.y-y*p.x)/p.norm());}
-    point& operator*=(const double&k){x*=k;y*=k;return *this;}
-    point& operator/=(const double&k){x/=k;y/=k;return *this;}
+    point& operator*=(const long double&k){x*=k;y*=k;return *this;}
+    point& operator/=(const long double&k){x/=k;y/=k;return *this;}
     point& operator+=(const point&p){x+=p.x;y+=p.y;return *this;}
     point& operator-=(const point&p){x-=p.x;y-=p.y;return *this;}
     bool operator==(const point&p){return equals(x,p.x)&&equals(y,p.y);}
-    double abs()const{return sqrt(norm());}
-    double norm()const{return x*x+y*y;}
-    static point polar(double r,double theta){return point(r*cos(theta),r*sin(theta));}
+    long double abs()const{return sqrt(norm());}
+    long double norm()const{return x*x+y*y;}
+    static point polar(long double r,long double theta){return point(r*cos(theta),r*sin(theta));}
   };
   struct segment{point s,t;segment(point s=0,point t=0):s(s),t(t){}};
   struct line{point s,t;line(point s=0,point t=0):s(s),t(t){}};
-  struct circle{point c;double r;circle(point c=0,double r=0):c(c),r(r){}};
+  struct circle{point c;long double r;circle(point c=0,long double r=0):c(c),r(r){}};
   struct polygon{
     int n;
     vector<point>ps;
@@ -37,9 +37,9 @@ namespace geometry{
   };
   ostream &operator<<(ostream&os,const point&p){return os<<p.x<<" "<<p.y;}
   istream &operator>>(istream&is,point&p){return is>>p.x>>p.y;}
-  double cross(point a,point b){return a.x*b.y-a.y*b.x;}
-  double dot(point a,point b){return a.x*b.x+a.y*b.y;}
-  point rotate(point o=point(0,0),point p,double theta){
+  long double cross(point a,point b){return a.x*b.y-a.y*b.x;}
+  long double dot(point a,point b){return a.x*b.x+a.y*b.y;}
+  point rotate(point p,long double theta,point o=point(0,0)){
     point pp=p-o;
     point res(pp.x*cos(theta)-pp.y*sin(theta),pp.x*sin(theta)+pp.y*cos(theta));
     return res+o;
@@ -70,59 +70,59 @@ namespace geometry{
   point project(point p,line l){return project(l,p);}
   point reflect(line l,point p){return project(l,p)*2-p;}
   point reflect(point p,line l){return reflect(l,p);}
-  double dist(point p,point q){return (p-q).abs();}
-  double dist(line l,point p){return dist(p,project(l,p));}
-  double dist(point p,line l){return dist(p,project(l,p));}
-  double dist(line l,line m){
+  long double dist(point p,point q){return (p-q).abs();}
+  long double dist(line l,point p){return dist(p,project(l,p));}
+  long double dist(point p,line l){return dist(p,project(l,p));}
+  long double dist(line l,line m){
     if(line_place(l,m)!=2)return 0;
     return dist(l,m.s);
   }
-  double dist(line l,segment s){
+  long double dist(line l,segment s){
     if(intersect(l,s))return 0;
     return min(dist(l,s.s),dist(l,s.t));
   }
-  double dist(segment s,line l){return dist(l,s);}
-  double dist(segment s,point p){
+  long double dist(segment s,line l){return dist(l,s);}
+  long double dist(segment s,point p){
     point q=project(line(s.s,s.t),p);
     if(intersect(s,q))return (p-q).abs();
     else return min((s.s-p).abs(),(s.t-p).abs());
   }
-  double dist(point p,segment s){return dist(p,s);}
-  double dist(segment s,segment t){
+  long double dist(point p,segment s){return dist(p,s);}
+  long double dist(segment s,segment t){
     if(intersect(s,t))return 0;
     return min(min(dist(s,t.s),dist(s,t.t)),min(dist(t,s.s),dist(t,s.t)));
   }
   point crosspoint(line l,line m){
-    double a=cross(m.t-m.s,m.s-l.s);
-    double b=cross(m.t-m.s,l.t-l.s);
+    long double a=cross(m.t-m.s,m.s-l.s);
+    long double b=cross(m.t-m.s,l.t-l.s);
     if(sign(a)==0&&sign(b)==0)return l.s;
     else if(sign(b)==0)throw "no crosspoint";
     return l.s+(l.t-l.s)*(a/b);
   }
-  double dist(circle c,point p,bool inside_zero=0){
-    if(inside_zero)return max(dist(c.c,p)-c.r,0.0);
-    return max(abs(dist(c.c,p)-c.r),0.0); 
+  long double dist(circle c,point p,bool inside_zero=0){
+    if(inside_zero)return max(dist(c.c,p)-c.r,(long double)0.0);
+    return max(abs(dist(c.c,p)-c.r),(long double)0.0); 
   }
-  double dist(circle c,line l){return max(dist(l,c.c)-c.r,0.0);}
-  double dist(circle c,segment s,bool inside_zero=0){
-    double sqr1=(s.s-c.c).norm(),sqr2=(s.t-c.c).norm();
+  long double dist(circle c,line l){return max(dist(l,c.c)-c.r,(long double)0.0);}
+  long double dist(circle c,segment s,bool inside_zero=0){
+    long double sqr1=(s.s-c.c).norm(),sqr2=(s.t-c.c).norm();
     if(inside_zero==0){
       if((sqr1<c.r*c.r)^(sqr2<c.r*c.r))return 0;
       else if(sqr1<c.r&&sqr2<c.r)return c.r-sqrt(max(sqr1,sqr2));
-      return max(dist(s,c.c)-c.r,0.0);
+      return max(dist(s,c.c)-c.r,(long double)0.0);
     }
     else{
       if(sqr1<c.r*c.r||sqr2<c.r*c.r)return 0;
-      return max(dist(s,c.c)-c.r,0.0);
+      return max(dist(s,c.c)-c.r,(long double)0.0);
     }
   }
-  double dist(circle c,circle d){
-    double dir=(c.c-d.c).abs();
-    return sign(dir-abs(c.r-d.r))==1?max(dir-c.r-d.r,0.0):abs(c.r-d.r)-dir;
+  long double dist(circle c,circle d){
+    long double dir=(c.c-d.c).abs();
+    return sign(dir-abs(c.r-d.r))==1?max(dir-c.r-d.r,(long double)0.0):abs(c.r-d.r)-dir;
   }
-  double dist(point p,circle c,bool inside_zero=0){return dist(c,p,inside_zero);}
-  double dist(line l,circle c){return dist(c,l);}
-  double dist(segment s,circle c,bool inside_zero=0){return dist(c,s,inside_zero);}
+  long double dist(point p,circle c,bool inside_zero=0){return dist(c,p,inside_zero);}
+  long double dist(line l,circle c){return dist(c,l);}
+  long double dist(segment s,circle c,bool inside_zero=0){return dist(c,s,inside_zero);}
   bool intersect(circle c,point p){return sign(dist(c,p))==0;}
   bool intersect(circle c,line l){return sign(dist(c,l))==0;}
   bool intersect(circle c,segment s){return sign(dist(c,s))==0;}
@@ -134,8 +134,8 @@ namespace geometry{
     vector<point>ret;
     if(sign(dist(c,d))!=0)return ret;
     point cd=d.c-c.c;
-    double di=cd.abs();
-    double x=(cd.norm()-d.r*d.r+c.r*c.r)/((double)2.0*di);
+    long double di=cd.abs();
+    long double x=(cd.norm()-d.r*d.r+c.r*c.r)/((long double)2.0*di);
     point inter=c.c+cd*(x/di);
     point rev=cd*point(0,sqrt(c.r*c.r-x*x)/di);
     ret.push_back(inter+rev);
@@ -144,11 +144,67 @@ namespace geometry{
   }
   vector<point>tangentpoint(circle c,point p){
     vector<point>ret;
-    double sin=c.r/(p-c.c).abs();
+    long double sin=c.r/(p-c.c).abs();
     if(sign(sin-1.0)==0)return ret;
-    double theta=pi/2.0-asin(min(sin,double(1.0)));
+    long double theta=pi/2.0-asin(min(sin,(long double)1.0));
     ret.push_back(c.c+(p-c.c)*point::polar(sin,theta));
-    if(sign(sin-double(1.0))!=0)ret.push_back(c.c+(p-c.c)*point::polar(sin,-theta));
+    if(sign(sin-(long double)1.0)!=0)ret.push_back(c.c+(p-c.c)*point::polar(sin,-theta));
     return ret;
+  }
+  point circumcenter(polygon pol){
+    assert(pol.n==3);
+    pol[0]=(pol[0]-pol[2])/2.0;
+    pol[1]=(pol[1]-pol[2])/2.0;
+    return pol[2]+crosspoint(line(pol[0],pol[0]*point(1,1)),line(pol[1],pol[1]*point(1,1)));
+  }
+  point circumcenter(point a,point b,point c){
+    return circumcenter(polygon(vector<point>{a,b,c}));
+  }
+  point center_of_gravity(polygon pol){
+    assert(pol.n==3);
+    return (pol[0]+pol[1]+pol[2])/3.0;
+  }
+  point center_of_gravity(point p1,point p2,point p3){
+    return center_of_gravity(polygon(vector<point>{p1,p2,p3}));
+  }
+  point innercenter(polygon pol){
+    assert(pol.n==3);
+    long double a=dist(pol[0],pol[1]),b=dist(pol[1],pol[2]),c=dist(pol[2],pol[0]);
+    return (pol[0]*a+pol[1]*b+pol[2]*c)/(a+b+c);
+  }
+  point innercenter(point p1,point p2,point p3){
+    return innercenter(polygon(vector<point>{p1,p2,p3}));
+  }
+  point orthocenter(polygon pol){
+    assert(pol.n==3);
+    return pol[0]+pol[2]+pol[3]-center_of_gravity(pol)*2.0;
+  }
+  point orthocenter(point p1,point p2,point p3){
+    return orthocenter(polygon(vector<point>{p1,p2,p3}));
+  }
+  long double area(polygon&pol){
+    long double ret=0;
+    for(int i=0;i<pol.n;i++)ret+=cross(pol[i],pol[(i+1)%pol.n]);
+    return ret/2.0;
+  }
+  long double isconvex(polygon&pol){
+    int neg=0,pos=0;
+    for(int i=0;i<pol.n;i++){
+      int dir=ccw(pol[i],pol[(i+1)%pol.n],pol[(i+2)%pol.n]);
+      if(dir==1)pos++;
+      else if(dir==-1)neg++;
+    }
+    if(pos&&neg)return 0;
+    return 1;
+  }
+  int in_polygon(point p,polygon&pol){
+    int n=pol.n;
+    bool in=false;
+    for(int i=0;i<n;i++){
+      point a=pol[i],b=pol[(i+1)%n];
+      if(intersect(p,segment(a,b)))return 2;
+      if(intersect(segment(p,point(p.x+1.0,INT_MAX)),segment(a,b)))in=!in; 
+    }
+    return in;
   }
 };
