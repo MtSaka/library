6b6b58bb881a0ad512541bb87ee799075fce15e4
MtSaka <mtsakachin@gmail.com>
2022-03-22 22:15:18 +0000

    add kth root integer

---
+++ test/yosupo/kth_root_integer.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/kth_root_integer"
+#include"../../template/template.hpp"
+#include"../../Math/number/kth_root_integer.hpp"
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    uint64_t a;
+    int k;
+    cin>>a>>k;
+    cout<<kth_root_integer(a,k)<<endl;
+  }
