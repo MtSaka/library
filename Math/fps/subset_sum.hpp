b58e8177e58ea8e6413c3d907eed07a559c6b73c
MtSaka <mtsakachin@gmail.com>
2022-03-25 21:42:28 +0000

    add subset sum

---
+++ Math/fps/subset_sum.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include"fps.hpp"
+template<long long Mod>
+FPS<Mod>subset_sum(vector<modint<Mod>>&count){
+  const int n=count.size();
+  vector<modint<Mod>>inv_table(n);
+  inv_table[1]=1;
+  for(int i=2;i<n;i++)inv_table[i]=-inv_table[mod%i]*(mod/i);
+  FPS<Mod>f(n);
+  for(int i=1;i<n;i++)if(count[i].x){
+    for(int j=1,k=i;k<n;j++,k+=i){
+      if(j&1)f[k]+=inv_table[j]*count[i];
+      else f[k]-=inv_table[j]*count[i];
+    }
+  }
+  return f.exp();
+}
---
+++ test/yosupo/sharp_p_subset_sum.test.cpp
@@ -0,0 +1,13 @@
+#include"../../template/template.hpp"
+#include"../../Math/fps/subset_sum.hpp"
+int main(){
+  int n,t;
+  cin>>n>>t;
+  vector<modint<mod>>cnt(t+1);
+  while(n--){
+    int s;cin>>s;
+    cnt[s]+=1;
+  }
+  auto ans=subset_sum(cnt);
+  for(int i=1;i<=t;i++)cout<<ans[i]<<" \n"[i==t];
+}
