5bac28afc46437aa3723f008a9c21b5547142782
MtSaka <mtsakachin@gmail.com>
2022-06-02 21:02:47 +0100

    fix verification

--- test/yosupo/staticrmq2.test.cpp
+++ test/yosupo/staticrmq2.test.cpp
@@ -1,14 +1,14 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
 #include"../../template/template.hpp"
 #include"../../Data_Structure/disjoint_sparse_table.hpp"
+int op(int a,int b){return min(a,b);}
+int e(){return INF<int>;}
 int main(){
   int n,q;
   cin>>n>>q;
   vi a(n);
   cin>>a;
-  auto f=[](int a,int b){return min(a,b);};
-  auto op=[](){return INF<int>;};
-  disjoint_sparse_table<int,f,op>st(a);
+  disjoint_sparse_table<int,op,e>st(a);
   while(q--){
     int l,r;
     cin>>l>>r;
