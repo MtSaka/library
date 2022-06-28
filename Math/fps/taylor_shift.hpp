fd16af243c5dc61c8360ea2c290abc6392fd8242
MtSaka <mtsakachin@gmail.com>
2022-06-28 02:27:43 +0100

    change a bit

--- Math/fps/taylor_shift.hpp
+++ Math/fps/taylor_shift.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include"fps.hpp"
 #include"../others/combinatorics.hpp"
-template<long long m>
-FPS<m>taylor_shift(FPS<m>f,modint<m>a,combination<m>&c){
+template<long long Mod>
+FPS<Mod>taylor_shift(FPS<Mod>f,modint<Mod>a,combination<Mod>&c){
   const int n=f.size();
   for(int i=0;i<n;i++)f[i]*=c.fac(i);
   reverse(f.begin(),f.end());
-  FPS<m>g(n,1);
+  FPS<Mod>g(n,1);
   for(int i=1;i<n;i++)g[i]=g[i-1]*a*c.fac(i-1)*c.finv(i);
   f*=g;
   f.resize(n);
