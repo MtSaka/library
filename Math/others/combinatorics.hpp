d49cc9d79a2110f17dd94ad64fca0bbf5fa049e8
MtSaka <mtsakachin@gmail.com>
2022-06-30 23:24:40 +0100

    change combination

--- Math/others/combinatorics.hpp
+++ Math/others/combinatorics.hpp
@@ -3,33 +3,47 @@
 template<long long m>
 struct combination{
   using mint=modint<m>;
-  vector<mint>dat,idat;
-  long long mx;
-  combination(long long mx_=300000):dat(mx_+1,1),idat(mx_+1,1),mx(mx_){
-    for(long long i=1;i<=mx;i++)dat[i]=dat[i-1]*mint(i);
-    idat[mx]/=dat[mx];
-    for(long long i=mx;i>0;i--)idat[i-1]=idat[i]*mint(i);
+  private:
+  static vector<mint>dat,idat;
+  inline static void extend(int sz){
+    if((int)dat.size()<sz+1){
+      int pre_sz=dat.size();
+      dat.resize(sz+1,1);
+      idat.resize(sz+1,1);
+      for(int i=pre_sz;i<=sz;i++)dat[i]=dat[i-1]*i;
+      idat[sz]/=dat[sz];
+      for(int i=sz-1;i>=pre_sz;i--)idat[i]=idat[i+1]*(i+1);
+    }
   }
-  mint com(long long n,long long k){
-    if(n<0||k<0||n<k)return mint(0);
-    return dat[n]*idat[k]*idat[n-k];
-  }
-  mint fac(long long n){
+  public:
+  combination():dat(1,1),idat(1,1){}
+  static inline mint fac(long long n){
     if(n<0)return mint(0);
+    extend(n);
     return dat[n];
   }
-  mint finv(long long n){
+  static inline mint finv(long long n){
     if(n<0)return mint(0);
+    extend(n);
     return idat[n];
   }
-  mint hom(long long n,long long k){
-    return com(n+k-1,k);
+  static mint com(long long n,long long k){
+    if(k<0||n<k)return mint(0);
+    return fac(n)*finv(k)*finv(n-k);
   }
-  mint per(long long n,long long k){
-    if(k<0||k>n)return mint(0);
-    return dat[n]*idat[n-k];
+  static mint hom(long long n,long long k){
+    if(n<0||k<0)return mint(0);
+    return k==0?1:com(n+k-1,k);
+  }
+  static mint per(long long n,long long k){
+    if(k<0||n<k)return mint(0);
+    return fac(n)*finv(n-k);
   }
 };
+template<long long m>
+vector<modint<m>>combination<m>::dat=vector<modint<m>>();
+template<long long m>
+vector<modint<m>>combination<m>::idat=vector<modint<m>>();
 template<long long p>
 struct COMB{
   vector<vector<modint<p>>>comb;
