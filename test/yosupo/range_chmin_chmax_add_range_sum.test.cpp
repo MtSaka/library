202eedca9d63a9bfec65991955d43b8c699bb0d4
MtSaka <mtsakachin@gmail.com>
2022-07-05 01:49:06 +0100

    add segtree_beats

---
+++ test/yosupo/range_chmin_chmax_add_range_sum.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_chmin_chmax_add_range_sum"
+#include"../../template/template.hpp"
+#include"../../Data_Structure/segtree_beats.hpp"
+int main(){
+  int n,q;cin>>n>>q;
+  vl a(n);cin>>a;
+  segtree_beats<ll>seg(a);
+  while(q--){
+    int t;cin>>t;
+    if(t==0){
+      int l,r;ll x;
+      cin>>l>>r>>x;
+      seg.range_chmin(l,r,x);
+    }
+    else if(t==1){
+      int l,r;ll x;
+      cin>>l>>r>>x;
+      seg.range_chmax(l,r,x);
+    }
+    else if(t==2){
+      int l,r;ll x;
+      cin>>l>>r>>x;
+      seg.range_add(l,r,x);
+    }
+    else{
+      int l,r;cin>>l>>r;
+      print(seg.range_sum(l,r));
+    }
+  }
+}
