a31b2b75f71dd95b2f0d96decd4d72d4dfce625c
MtSaka <mtsakachin@gmail.com>
2022-07-11 11:33:30 +0100

    fix modint

--- Math/fps/subset_sum.hpp
+++ Math/fps/subset_sum.hpp
@@ -7,7 +7,7 @@ FPS<Mod>subset_sum(vector<modint<Mod>>&count){
   inv_table[1]=1;
   for(int i=2;i<n;i++)inv_table[i]=-inv_table[mod%i]*(mod/i);
   FPS<Mod>f(n);
-  for(int i=1;i<n;i++)if(count[i].x){
+  for(int i=1;i<n;i++)if(count[i].val()){
     for(int j=1,k=i;k<n;j++,k+=i){
       if(j&1)f[k]+=inv_table[j]*count[i];
       else f[k]-=inv_table[j]*count[i];
