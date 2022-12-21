35569d4936c7ecd14c10ab73a42e0b6a7b755e40
MtSaka <mtsakachin@gmail.com>
2022-12-22 01:12:24 +0900

    fix primitive-root

--- math/number/primitive-root.hpp
+++ math/number/primitive-root.hpp
@@ -1,26 +1,41 @@
 #pragma once
 #include"../../template/template.hpp"
-#include"fast-prime.hpp"
+#include"../modular/montgomery-modint.hpp"
+#include"pollard-rho.hpp"
 
-int primitive_root(int n){
+template<typename T=MontgomeryModInt<ull,-4>,typename Rand=Random64>
+ull primitive_root(ull n,Rand rand=rand64){
+  assert(is_prime_fast(n));
   if(n==2)return 1;
-  if(n==167772161)return 3;
-  if(n==469762049)return 3;
-  if(n==754974721)return 11;
-  if(n==998244353)return 3;
-  if(n==1224736769)return 3;
-  auto divs=fastprime::factorize(n-1);
+  if(T::get_mod()!=n)T::set_mod(n);
+  auto divs=factorize(n-1);
   divs.erase(unique(divs.begin(),divs.end()),divs.end());
-  for(int g=2;;g++){
-    bool ok=true;
-    for(int d:divs){
-      if(mod_pow(g,(n-1)/d,n)==1){
+  for(auto&x:divs)x=(n-1)/x;
+  const T e=1;
+  while(1){
+    ull g=rand.uniform(2ull,n-1);
+    bool ok=1;
+    for(auto x:divs){
+      if(T(g).pow(x)==e){
         ok=false;
         break;
       }
     }
     if(ok)return g;
   }
+}
+template<ull p,enable_if_t<is_prime_v<p>>* =nullptr>
+constexpr ull constexpr_primitive_root(){
+  if(p==2)return 1;
+  if(p==167772161)return 3;
+  if(p==469762049)return 3;
+  if(p==754974721)return 11;
+  if(p==998244353)return 3;
+  if(p==1224736769)return 3;
+  rep(g,2,p){
+    if(mod_pow(g,(p-1)>>1,p)!=1)return g;
+  }
+  return -1;
 }
 /**
  * @brief Primitive Root(原始根)
