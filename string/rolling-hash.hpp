6d0d5b2b3900c46a53cc7fda05e3d970cb3768ae
MtSaka <mtsakachin@gmail.com>
2023-01-27 16:25:04 +0900

    add lcp to rolling hash

--- string/rolling-hash.hpp
+++ string/rolling-hash.hpp
@@ -26,14 +26,14 @@ struct RollingHash{
     BASE=(1ull<<31)+(random_device()()&MASK31);
   }
   public:
-  struct hash{
+  struct Hash{
     private:
     int n;
     ull BASE;
     vector<ull>inner_hash,power;
     public:
     template<typename T>
-    hash(ull base,const T&s):BASE(base){
+    Hash(ull base,const T&s):BASE(base){
       n=s.size();
       inner_hash.resize(n+1);
       for(int i=0;i<n;i++)inner_hash[i+1]=calc_add(s[i],calc_mul(BASE,inner_hash[i]));
@@ -46,11 +46,24 @@ struct RollingHash{
     ull get_all()const{
       return inner_hash[n];
     }
+    size_t size()const{return n;}
   };
   RollingHash(){init();}
   template<typename T>
-  hash get_hash(const T&s)const{return hash(BASE,s);}
+  Hash get_hash(const T&s)const{return Hash(BASE,s);}
   ull get_base()const{return BASE;}
+  int lcp(const Hash&h1,const Hash&h2,int l1=0,int r1=-1,int l2=0,int r2=-1){
+    if(r1==-1)r1=h1.size();
+    if(r2==-1)r2=h2.size();
+    const int len=min(r1-l1,r2-l2);
+    int ok=0,ng=len+1;
+    while(ng-ok>1){
+      int mid=(ok+ng)>>1;
+      if(h1.get_hash(l1,l1+mid)==h2.get_hash(l2,l2+mid))ok=mid;
+      else ng=mid;
+    }
+    return ok;
+  }
 };
 /**
  * @brief Rolling Hash(ローリングハッシュ)
---
+++ test/yosupo/string/zalgorithm.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+#include"../../template/template.hpp"
+#include"../../string/rolling-hash.hpp"
+int main(){
+  RollingHash rh;
+  STR(s);
+  const int n=s.size();
+  auto h=rh.get_hash(s);
+  vi ans(n);
+  rep(i,n)ans[i]=rh.lcp(h,h,0,n,i,n);
+  print(ans);
+}
