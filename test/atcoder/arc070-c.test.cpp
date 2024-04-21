881e08429ddab58f5d447ca1a851fd2747e2018a
MtSaka <mtsakachin@gmail.com>
2024-04-21 15:40:42 +0900

    add slope-trick

---
+++ test/atcoder/arc070-c.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://atcoder.jp/contests/arc070/tasks/arc070_e"
+#include"../../template/template.hpp"
+#include"../../ds/others/slope-trick.hpp"
+
+int main(){
+    INT(n);
+    vp a(n);cin>>a;
+    SlopeTrick<ll> st;
+    st.addabs(a[0].first);
+    rep(i,1,n){
+        st.sliding_window(a[i].first-a[i].second,a[i-1].second-a[i-1].first);
+        st.addabs(a[i].first);
+    }
+    print(st.getmin());
+}
