7c4e2a6109cd2982b176806b94ff2a5ffadcc755
MtSaka <mtsakachin@gmail.com>
2024-04-21 15:14:33 +0900

    add manacher

---
+++ test/yosupo/string/enumerate_palindromes.test.cpp
@@ -0,0 +1,8 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_palindromes"
+#include"../../../template/template.hpp"
+#include"../../../string/manacher.hpp"
+
+int main(){
+    string s;cin>>s;
+    print(manacher(s));
+}
