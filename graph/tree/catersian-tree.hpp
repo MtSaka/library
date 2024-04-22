2e518e87c2ad044e27146b4ae8a9f205760efaff
mtsaka <mtsakachin@gmail.com>
2024-04-22 10:17:54 +0900

    add catersian-tree

---
+++ graph/tree/catersian-tree.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include"../../template/template.hpp"
+#include"../graph-template.hpp"
+
+template<typename T,typename Cmp=less<T>>
+struct CatersianTree{
+    private:
+    int n;
+    const vector<T>&v;
+    vector<int>par;
+    Cmp cmp;
+    void init(){
+        n=v.size();
+        par.assign(n,-1);
+        rep(i,1,n){
+            int p=i-1,lst=-1;
+            while(p!=-1&&cmp(v[i],v[p])){
+                lst=p;
+                p=par[p];
+            }
+            if(lst!=-1)par[lst]=i;
+            par[i]=p;
+        }
+    }
+    public:
+    CatersianTree(const vector<T>&v):v(v),cmp(Cmp()){init();}
+    CatersianTree(const vector<T>&v,const Cmp&cmp):v(v),cmp(cmp){init();}
+    vector<int>get()const{return par;}
+};
---
+++ test/yosupo/tree/catersian_tree.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/cartesian_tree"
+#include"../../../template/template.hpp"
+#include"../../../graph/tree/catersian-tree.hpp"
+
+int main(){
+    int n;cin>>n;
+    vector<int>a(n);cin>>a;
+    auto v=CatersianTree(a).get();
+    rep(i,n)if(v[i]==-1)v[i]=i;
+    print(v);
+}
