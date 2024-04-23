69e447d826a3efb78267f74e2955977f182cf6b6
mtsaka <mtsakachin@gmail.com>
2024-04-23 10:33:33 +0900

    add fast-set

---
+++ ds/others/fast-set.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include"../../template/template.hpp"
+
+template<int B=64>
+struct FastSet{
+    private:
+    using u64=uint64_t;
+    u64 n;
+    vector<vector<u64>>st;
+    public:
+    FastSet(int n_):n(n_){
+        do{
+            st.emplace_back(vector<u64>((n_+B-1)/B));
+            n_=(n_+B-1)/B;
+        }while(n_>1);
+    }
+    void insert(int k){
+        for(auto&v:st){
+            v[k/B]|=1ull<<(k%B);
+            k/=B;
+        }
+    }
+    void erase(int k){
+        for(auto&v:st){
+            v[k/B]&=~(1ull<<(k%B));
+            if(v[k/B])break;
+            k/=B;
+        }
+    }
+    bool contains(int k){
+        return (st[0][k/B]>>(k%B))&1;
+    }
+    int next(int k){
+        rep(h,st.size()){
+            k++;
+            if(k/B>=(int)st[h].size())break;
+            u64 d=st[h][k/B]>>(k%B);
+            if(d){
+                k+=countr_zero(d);
+                while(h--)k=k*B+countr_zero(st[h][k]);
+                return k;
+            }
+            k/=B;
+        }
+        return n;
+    }
+    int prev(int k){
+        rep(h,st.size()){
+            k--;
+            if(k<0)break;
+            u64 d=st[h][k/B]<<(~k % B);
+            if(d){
+                k-=countl_zero(d);
+                while(h--)k=k*B+B-countl_zero(st[h][k]);
+                return k;
+            }
+            k/=B;
+        }
+        return -1;
+    }
+};
+/**
+ * @brief Fast Set
+*/
---
+++ test/yosupo/data_strucuture/predecessor_problem.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/predecessor_problem"
+#include"../../../template/template.hpp"
+#include"../../../ds/others/fast-set.hpp"
+
+int main(){
+    INT(n,q);
+    FastSet st(n);
+    STR(s);
+    rep(i,n)if(s[i]=='1')st.insert(i);
+    while(q--){
+        INT(t,k);
+        if(t==0){
+            if(!st.contains(k))st.insert(k);
+        }
+        else if(t==1){
+            if(st.contains(k))st.erase(k);
+        }
+        else if(t==2){
+            print(st.contains(k));
+        }
+        else if(t==3){
+            int ans=st.next(k-1);
+            if(ans==n)ans=-1;
+            print(ans);
+        }
+        else{
+            print(st.prev(k+1));
+        }
+    }
+}
