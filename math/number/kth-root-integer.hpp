9fca6daaafc1bf1652665a3470d665f1d7951d24
MtSaka <mtsakachin@gmail.com>
2024-07-08 19:46:19 +0900

    change implementation of kth root integer

--- math/number/kth-root-integer.hpp
+++ math/number/kth-root-integer.hpp
@@ -2,16 +2,24 @@
 #include "../../template/template.hpp"
 
 ull kth_root_integer(ull a, int k) {
-    if (k == 1) return a;
+    if (a == 0 || a == 1 || k == 1) return a;
+    if (k >= 64) return 1;
+    if (k == 2) return sqrtl(a);
     auto check = [&](ull x) {
         ull mul = 1;
-        for (int j = 0; j < k; j++) {
-            if (__builtin_mul_overflow(mul, x, &mul)) return false;
+        int now = k;
+        for (; now;) {
+            if (now & 1) {
+                if (__builtin_mul_overflow(mul, x, &mul)) return false;
+            }
+            if (now >>= 1) {
+                if (__builtin_mul_overflow(x, x, &x)) return false;
+            }
         }
         return mul <= a;
     };
     ull ret = 0;
-    for (int i = 31; i >= 0; i--) {
+    for (int i = 21; i >= 0; i--) {
         if (check(ret | (1u << i))) ret |= 1u << i;
     }
     return ret;
