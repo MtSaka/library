3932ad79fbaa5e393e26576feb0c4461d1d6caa4
MtSaka <mtsakachin@gmail.com>
2024-07-17 19:16:37 +0900

    change to arbitrary modint

--- math/modular/mod-sqrt.hpp
+++ math/modular/mod-sqrt.hpp
@@ -1,29 +1,36 @@
 #pragma once
 #include "../../template/template.hpp"
+#include "montgomery-modint.hpp"
+#include "../number/primitive-root.hpp"
 
-ll mod_sqrt(ll a, const ll& p) {
-    a %= p;
-    if (a == 0) return 0;
-    if (p == 2) return a;
-    if (mod_pow(a, (p - 1) / 2, p) != 1) return -1;
-    if ((p & 3) == 3) return mod_pow(a, (p + 1) / 4, p);
-    ll q = p - 1, s = 0, z = 2;
-    while (!(q & 1)) q >>= 1, s++;
-    while (mod_pow(z, (p - 1) / 2, p) == 1) z++;
-    ll m = s, c = mod_pow(z, q, p), t = mod_pow(a, q, p), r = mod_pow(a, (q + 1) / 2, p);
-    while (t != 1) {
-        ll pow_t = t * t % p, m_update = 0;
+ll mod_sqrt(const ll& a_, const ll& p) {
+    if (a_ % p == 0) return 0;
+    if (p == 2) return a_ % p;
+    using mint = ArbitraryModInt;
+    mint::set_mod(p);
+    const mint a = a_;
+    if (a.pow((p - 1) / 2) != mint(1)) return -1;
+    if ((p & 3) == 3) return a.pow((p + 1) / 4).get();
+    ll q = p - 1;
+    int m = 0;
+    mint z = primitive_root(p);
+    while (!(q & 1)) q >>= 1, m++;
+    mint c = z.pow(q), t = a.pow(q), r = a.pow((q + 1) / 2);
+    while (t != mint(1)) {
+        mint pow_t = t * t;
+        int m_update = 0;
         for (int j = 1; j < m; j++) {
-            if (pow_t == 1) {
+            if (pow_t == mint(1)) {
                 m_update = j;
                 break;
             }
-            pow_t = pow_t * pow_t % p;
+            pow_t *= pow_t;
         }
-        ll b = mod_pow(c, 1LL << (m - m_update - 1), p);
-        m = m_update, c = mod_pow(b, 2, p), t = (t * b % p) * b % p, r = r * b % p;
+        const mint b = c.pow(1LL << (m - m_update - 1));
+        m = m_update;
+        c = b * b, t *= c, r *= b;
     }
-    return r;
+    return r.get();
 }
 /**
  * @brief Mod Square Root(平方剰余)
