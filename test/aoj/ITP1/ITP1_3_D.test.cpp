daccc8bd0e716ebcae805022b89705375fb086d7
mtsaka <mtsakachin@gmail.com>
2024-10-28 08:24:41 +0900

    fix

--- test/aoj/ITP1/ITP1_3_D.test.cpp
+++ test/aoj/ITP1/ITP1_3_D.test.cpp
@@ -1,11 +1,11 @@
 // competitive-verifier: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_D
-#include "../../../math/number/factor.hpp"
+#include "../../../math/number/divisor.hpp"
 #include "../../../template/template.hpp"
 int main() {
     int a, b, c;
     sc >> a >> b >> c;
     int ans = 0;
-    for (auto& x : factor(c)) {
+    for (auto& x : divisor(c)) {
         ans += (a <= x && x <= b);
     }
     print(ans);
