ee726ddc1d7800d90032ad2564c5c120a4c8f8d2
MtSaka <mtsakachin@gmail.com>
2024-10-31 22:44:53 +0900

    add maximum independent set

---
+++ graph/others/maximum-independet-set.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "../../template/template.hpp"
+ull MaximumIndependentSet(const vector<vi>& g) {
+    const int n = g.size();
+    vector<ull> adj(n);
+    rep(i, n) for (auto& e : g[i]) adj[i] |= 1ull << e;
+    int ans = 0;
+    ull res = 0;
+    auto dfs = REC([&](auto&& dfs, ull now, ull rest) -> void {
+        pair<int, int> p = {-1, -1};
+        while (true) {
+            bool f = false;
+            rep(v, n) if ((rest >> v) & 1) {
+                int deg = popcount(adj[v] & rest);
+                chmax(p, pair<int, int>{deg, v});
+                if (deg <= 1) {
+                    rest ^= 1ull << v, rest &= ~adj[v], now |= 1ull << v;
+                    f = true;
+                }
+            }
+            if (!f) break;
+            p = {-1, -1};
+        }
+        const int sz1 = popcount(now), sz2 = popcount(rest);
+        if (chmax(ans, sz1)) res = now;
+        if (sz2 == 0 || sz1 + sz2 <= ans) return;
+        const int v = p.second;
+        rest &= ~(1ull << v);
+        dfs(now, rest);
+        now |= 1ull << v;
+        dfs(now, rest & (~adj[v]));
+    });
+    dfs(0, (1ull << n) - 1);
+    return res;
+}
+/**
+ * @brief Maximum Independent Set
+ */
---
+++ test/yosupo/graph/maximum_independent_set.test.cpp
@@ -0,0 +1,16 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/maximum_independent_set
+#include "../../../template/template.hpp"
+#include "../../../graph/others/maximum-independet-set.hpp"
+int main() {
+    INT(n, m);
+    vector<vi> g(n);
+    rep(m) {
+        INT(u, v);
+        g[u].eb(v);
+        g[v].eb(u);
+    }
+    auto ans = MaximumIndependentSet(g);
+    print(popcount(ans));
+    rep(i, n) if ((ans >> i) & 1) pr << i << " ";
+    pr << endl;
+}
