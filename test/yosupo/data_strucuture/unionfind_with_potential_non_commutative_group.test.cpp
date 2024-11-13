7b13cd400ddc4e4fc8586c89ecf7f1e0920e284a
mtsaka <mtsakachin@gmail.com>
2024-11-14 08:53:04 +0900

    fix

--- test/yosupo/data_strucuture/unionfind_with_potential_non_commutative_group.test.cpp
+++ test/yosupo/data_strucuture/unionfind_with_potential_non_commutative_group.test.cpp
@@ -1,3 +1,4 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/unionfind_with_potential_non_commutative_group
 #include "../../../template/template.hpp"
 #include "../../../ds/union-find/weighted-union-find.hpp"
 #include "../../../math/modular/modint.hpp"
