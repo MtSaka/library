976b81d747ec8a71ccefb2bec6129e5f7d2dc071
mtsaka <mtsakachin@gmail.com>
2024-11-13 11:58:25 +0900

    add test

---
+++ test/yosupo/data_strucuture/point_set_range_composite_large_array.test.cpp
@@ -0,0 +1,31 @@
+// cp-verifier: PROBLEM https://judge.yosupo.jp/problem/point_set_range_composite_large_array
+#include "../../../template/template.hpp"
+#include "../../../ds/segment/segment-tree.hpp"
+#include "../../../math/modular/modint.hpp"
+#include "../../../others/monoid2.hpp"
+using mint = ModInt<998244353>;
+using T = pair<mint, mint>;
+
+int main(){
+    INT(n, q);
+    vector<array<int,4>>query(q);
+    compressor<int>press;
+    for(auto&qu:query) {
+        sc >> qu;
+        if(qu[0] == 0) press.push_back(qu[1]);
+    }
+    press.build();
+    SegmentTree<Monoid::Composite<mint>> seg(press.size());
+    for(auto&qu:query) {
+        if(qu[0] == 0) {
+            int id = press.get_index(qu[1]);
+            seg.set(id, T{qu[2], qu[3]});
+        }
+        else {
+            const int l = press.get_index(qu[1]);
+            const int r = press.get_index(qu[2]);
+            const auto [a, b] = seg.prod(l,r);
+            print(a * qu[3] + b);
+        }
+    }
+}
