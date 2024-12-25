4c8ee4cf61ca67679123c2ad3828e5f0211bcaf0
MtSaka <mtsakachin@gmail.com>
2024-12-26 00:14:03 +0900

    change problem 3030 to problem 8080(yukicoder)

--- test/yukicoder/8030.test.cpp
+++ test/yukicoder/8030.test.cpp
@@ -1,4 +1,4 @@
-// competitive-verifier: PROBLEM https://yukicoder.me/problems/no/3030
+// competitive-verifier: PROBLEM https://yukicoder.me/problems/no/8030
 #include "../../math/number/miller-rabin.hpp"
 #include "../../template/template.hpp"
 int main() {
