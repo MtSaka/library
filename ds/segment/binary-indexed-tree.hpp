362c37123817b179a13ddfacb2b069f2a8eac28d
MtSaka <mtsakachin@gmail.com>
2026-05-29 00:59:42 +0900

    add lb,ub for bit

--- ds/segment/binary-indexed-tree.hpp
+++ ds/segment/binary-indexed-tree.hpp
@@ -5,6 +5,8 @@
 template <typename M, bool = Monoid::is_monoid<M>::value>
 struct BinaryIndexedTree {
    private:
+    template <typename, bool>
+    friend struct BinaryIndexedTree;
     using T = typename M::value_type;
     int n;
     vector<T> data;
@@ -13,6 +15,7 @@ struct BinaryIndexedTree {
     BinaryIndexedTree() : BinaryIndexedTree(0) {}
     BinaryIndexedTree(int n_) { init(n_); }
     BinaryIndexedTree(const vector<T>& v) {
+        n = (int)v.size();
         data.clear();
         data.emplace_back(M::id());
         data.insert(data.end(), v.begin(), v.end());
@@ -54,6 +57,27 @@ struct BinaryIndexedTree<T, false> : BinaryIndexedTree<Monoid::Sum<T>> {
     void add(int k, T x) { this->apply(k, x); }
     T sum(int k) const { return this->prod(k); }
     T sum(int l, int r) const { return this->prod(l, r); }
+
+    int lower_bound(T x) const {
+        int i = 0;
+        for (int k = 1 << (__lg(this->n) + 1); k > 0; k >>= 1) {
+            if (i + k <= this->n && this->data[i + k] < x) {
+                x -= this->data[i + k];
+                i += k;
+            }
+        }
+        return i + 1;
+    }
+    int upper_bound(T x) const {
+        int i = 0;
+        for (int k = 1 << (__lg(this->n) + 1); k > 0; k >>= 1) {
+            if (i + k <= this->n && this->data[i + k] <= x) {
+                x -= this->data[i + k];
+                i += k;
+            }
+        }
+        return i + 1;
+    }
 };
 /**
  * @brief Binary Indexed Tree(Fenwick Tree, BIT)
